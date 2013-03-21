4ae1bb34fd79ceca362d70ef2fc07378325d0264
cuikc <cuikc@cuikc.com>
2013-03-21 17:07:40 +0800

    增加service-name参数配置、增加端口是否被占用判断： 增加根据参数修改service-name的功能，以及处理重复的参数配置行。 增加判断端口是否被占用，如果被占用则直接退出脚本，并返回相应提示信息。

--- dubbo-deploy.sh
+++ dubbo-deploy.sh
@@ -50,26 +50,67 @@ then
 	echo "dubbo.properties is here";
 	PORT_CONF='';
 	PORT_CONF=`egrep -c '^dubbo.protocol.port' "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties`;
-	if [[ "$PORT_CONF" == 0 ]]         #PORT_CONF没有找到port设置。
+	if [[ "$PORT_CONF" == 0 ]]		#PORT_CONF没有找到port设置，则在配置文件末尾添加一行端口参数。
 	then
-		echo "1"
+		echo "insert"
 		echo "$PORT_CONF";
 		sed -i '$a\dubbo.protocol.port='"${2}" "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties;
 	else					#如果有一个或多个port的设置，则替换为参数指定的端口号。
-		echo "2"
+		echo "change"
 		echo "$PORT_CONF";
 		echo "$2";
 		sed -i "s/^dubbo\.protocol\.port=.*/dubbo.protocol.port="$2"/" "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties;
+	fi
+	
+	NAME_CONF='';
+	NAME_CONF=`egrep -c '^dubbo.application.name=' "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties`
+	if [[ "$NAME_CONF" == 0 ]]		#如果没有service-name设置，则在配置文件末尾添加一行service-name参数。
+	then
+		echo "insert";
+		echo "$NAME_CONF";
+		sed -i '$a\dubbo.application.name='"${1}" "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties;
+	else					#如果有一个或多个service-name设置，则替换为参数指定的service-name。
+		echo "change";
+		echo "$NAME_CONF";
+		echo "$1";
+		sed -i "s/^dubbo\.application\.name=.*/dubbo.application.name="$1"/" "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties;
+		uniq "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties > "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties-uniq;
+		mv "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties-uniq "$DUBBO_PATH""$SERVICE_PATH"/conf/dubbo.properties;
 	fi
 else
 	echo "there is no dubbo.properties";
 fi
 
+###################################
 
 
+###################################
+#check the prot is useing or not
+###################################
 
+PORT_STATUS=`netstat -plnut | awk '{print $4}' | awk -F ":" '{print $2}' | \
+while read PORTS 
+do
+	#echo $PORTS;
+	#echo $2;
+	if [[ "$PORTS" == $2 ]]
+	then
+		#echo "The Port is in using by someone,Please choose another one,Thanks.";
+		echo "no";
+		break;
+	#else
+		#echo "OK";
+	fi
+done`;
 
+echo $PORT_STATUS;
 
+if [[ "$PORT_STATUS" == "no" ]]
+then
+	echo "The Port is in using by someone,Please choose another one,Thanks.";
+	exit;
+fi
+echo "check port complete";
 
 
 
